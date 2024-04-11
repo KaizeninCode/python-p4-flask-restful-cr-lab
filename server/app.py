d04ba1c0e1fbcffcc376ad5b33c4a7b14b47130d
Phil <philwekullo@gmail.com>
2024-04-11 05:56:31 +0300

    complete lab

--- server/app.py
+++ server/app.py
@@ -17,10 +17,47 @@ db.init_app(app)
 api = Api(app)
 
 class Plants(Resource):
-    pass
+    def get(self):
+        plants_dict_list = [plant.to_dict() for plant in Plant.query.all()]
+        return make_response(plants_dict_list, 200)
+
+    def post(self):
+        ## The post method now checks request.json instead of request.form since the test is sending JSON data.
+        
+         # Check if all required form fields are present
+        required_fields = ['name', 'image', 'price']
+        if not all(field in request.json for field in required_fields):
+            return make_response({'error': 'Missing required fields'}, 400)
+
+    # Check if any required field has a None value
+        if any(request.json[field] is None for field in required_fields):
+            return make_response({'error': 'Required fields cannot be None'}, 400)
+        
+        new_record = Plant(
+        name=request.json['name'],
+        image=request.json['image'],
+        price=request.json['price'],
+    )
+
+        db.session.add(new_record)
+        db.session.commit()
+
+        response_dict = new_record.to_dict()
+
+        return make_response(response_dict, 201)
+    
+api.add_resource(Plants, '/plants')    
 
 class PlantByID(Resource):
-    pass
+    def get(self, id):
+        plant = Plant.query.filter_by(id=id).first()
+        if plant:
+            response_dict = plant.to_dict()
+            return make_response(response_dict, 200)
+        else:
+            return make_response({'error': 'Plant not found'}, 404)
+    
+api.add_resource(PlantByID, '/plants/<int:id>')    
         
 
 if __name__ == '__main__':
